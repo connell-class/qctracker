6e90c1ddcccd5775b51558c5db89c199153e2ef4
beko1200 <berkantbulut91@gmail.com>
2020-02-06 16:55:14 -0500

    DoneForNow

--- qctracker/src/components/avg-grade-component/AvgGradeComponent.tsx
+++ qctracker/src/components/avg-grade-component/AvgGradeComponent.tsx
@@ -1,32 +1,31 @@
 import React from 'react';
 
 export class AvgGradeComponent extends React.Component <any,any> {
+    IndividualGrade: any;
+    numOfGrades: any;
+    StudentAverageGrade: any
 
     constructor(props:any){
         super(props)
         this.state = {
             IndividualGrade: Number,
             numOfGrades: Number,
-            AverageGrade: Number
+            studentAverageGrade: Number
         }
-
+    }
        
+        AverageGrade  = () => {
+            return  this.IndividualGrade * this.numOfGrades / this.numOfGrades
         }
-
-        const numOfGrades 
-
 
             render () {
                 return(
-                   
+
+                    < div id="AvgGrade">
+                      grade = {this.props.student.AverageGrade}
+                       studentAverageGrade = {this.AverageGrade()}
+                       </div> 
                 )
             }
-
-
-      // render() {
-//            return(
-//                
-//            )
- //     }
 
  }
