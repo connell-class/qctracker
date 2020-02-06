503c854ed6e75323749865c2d38db1eca98d974f
beko1200 <berkantbulut91@gmail.com>
2020-02-06 15:27:42 -0500

    committing

---
+++ qctracker/src/components/avg-grade-component/AvgGradeComponent.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export class AvgGradeComponent extends React.Component <any,any> {
+
+    constructor(props:any){
+        super(props)
+        this.state = {
+            IndividualGrade: Number,
+            numOfGrades: Number,
+            AverageGrade: Number
+        }
+
+       
+        }
+
+        const numOfGrades 
+
+
+            render () {
+                return(
+                   
+                )
+            }
+
+
+      // render() {
+//            return(
+//                
+//            )
+ //     }
+
+ }
