3bd0aefe8344595953b9e024ee4db342a5cf33a7
CarloAnselmo <CarloAnselmo@gmail.com>
2020-11-06 17:57:39 -0500

    good luck team

--- qctracker/src/App.tsx
+++ qctracker/src/App.tsx
@@ -1,56 +1,33 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import { JudgementContainer } from './components/judgements/JudgementContainer';
-
-
-import './App.css';
+import { Landing } from './components/landing/Landing';
 import { ProfilePage } from './components/profile/ProfilePage';
-
-
-function App() {
-  return (
-    <div className="App">
-
-      <JudgementContainer/>
-
-        <ProfilePage firstName="First" lastName="Last" email="email1"/>
-
-    </div>
-
-
-import './App.css';
-import { Landing } from './component/landing/Landing'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-import { LoginPage } from './pages/loginPage'
-import { RegisterPage } from './pages/registerPage'
-
-
-
+import { LoginPage } from './pages/loginPage';
+import { RegisterPage } from './pages/registerPage';
 
 function App() {
   return (
     <body>
     <Router>
       <Switch>
+        <Route exact path="/" component={RegisterPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/register" component={RegisterPage} />
-        <Route exact path="/" component={RegisterPage} />
         <Route path="/landing" component={Landing}/>
+        <Route path="/profilepage">
+          <ProfilePage {...JSON.parse(sessionStorage.getItem("user") || `{
+    "id": 6,
+    "email": "james@jammes.net",
+    "firstName": "James",
+    "lastName": "James",
+    "password": "wasspord"
+  }`)} />
+        </Route>
       </Switch>
     </Router>
     </body>
-
   );
-}
+};
 
 export default App;
-
-
-
