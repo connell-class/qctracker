bae1048d8bb9364ab3f592b6e7c1e184fa47a509
Colin-Schultz-Programming <64875328+Colin-Schultz-Programming@users.noreply.github.com>
2020-11-06 11:33:15 -0500

    updated stuff

--- qctracker/src/component/landing/Landing.tsx
+++ qctracker/src/component/landing/Landing.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { IState, IlandingButtonsState } from '../redux/reducers';
+
+
 import './landing.scss';
-import { LoginButton } from './login/Login Button';
-import { RegisterButton } from './register/Register Button';
+
 import { Register } from './register/Register';
-import { Login } from './login/Login';
-import logo from '../../resources/Rev Social.png';
+
+import logo from '../../REVAJUDGE.png';
 export const Landing: React.FC = () => {
   
-  const buttons = useSelector<IState, IlandingButtonsState>(state => state.landingButtonState);
+
   
   return (
     <body>
       <div className = "landingButtonsPanel inline">
-  
-        
-    
-     
+
         <img className = "imgLandingLogo" alt = "logo" src = {logo}/>
-        { buttons.showRegister && <Register/>}
-        { buttons.showLogin && <Login/>}
+       <Register/>
+      
         </div>
   
     </body>
--- qctracker/src/component/landing/register/Register.tsx
+++ qctracker/src/component/landing/register/Register.tsx
@@ -1,10 +1,6 @@
 import React, { SyntheticEvent } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { confirmPass, email, firstName, lastName, password } from '../../redux/actions/UserActions';
 import { Form, Input } from 'reactstrap';
-import { IState,IlandingButtonsState, IUserState } from '../../redux/reducers/index';
-import { API_CONN_ROOT } from '../../config/ApiConn';
-import { register } from '../../redux/actions/LandingButtonActions';
+
 
 
 
@@ -13,9 +9,7 @@ import { register } from '../../redux/actions/LandingButtonActions';
 export const Register: React.FC = () => {
 
   // Redux solution to tracking user state
-  const dispatch = useDispatch();
-  const user = useSelector<IState, IUserState>(state => state.userState);
-  const buttons = useSelector<IState, IlandingButtonsState>(state => state.landingButtonState);
+  
 
 
   // local state solution in lieu of Redux
@@ -28,46 +22,26 @@ export const Register: React.FC = () => {
   // })
 
   const handleSubmit = async (event:SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(event)
-    try{
-      const response = await fetch(`${API_CONN_ROOT}/where-ever it ends up`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify(user)
-      });
-      if(response.status === 200){
-        // dispatch JWT to store and use JWT to authenticate when ever the user makes a post/put/patch/delete request
-        // history.push(user's url) to automatically log the user in after account creation. Might not want to do this to all for the email confirmation.
-
-      } else {
-        // Consolelog/log the error if it is outside a taken username or email. Tell user if the username or email is taken
-      }
-    } catch(err){
-      console.log(err);
-    }
+  
   }
   
 
   return (
 
     <Form onSubmit={handleSubmit} className = "landingPageForms">
-      <Input type='text' name='firstName' placeholder='First name' onChange={e => dispatch(firstName(e.target.value))}/>
+      <Input type='text' name='firstName' placeholder='First name'/>
       <br></br>
-      <Input type='text' name='lastName' placeholder='Last name' onChange={e => dispatch(lastName(e.target.value))}/>
+      <Input type='text' name='lastName' placeholder='Last name' />
       <br></br>
-      <Input type='email' name='email' placeholder='Email address' onChange={e => dispatch(email(e.target.value))}/>
+      <Input type='email' name='email' placeholder='Email address' />
       <br></br>
-      <Input type='password' name='password' placeholder='New password' onChange={e => dispatch(password(e.target.value))}/>
+      <Input type='password' name='password' placeholder='New password' />
       <br></br>
-      <Input type='password' name='confirm' placeholder='Confirm password' onChange={e => dispatch(confirmPass(e.target.value))}/>
+      <Input type='password' name='confirm' placeholder='Confirm password'/>
       <br></br>
       <Input type='submit' value='Register' className = "btn"/>
       <br></br>
-      <Input type ='reset' value = 'Close' className = "btn cancel" onClick={e => dispatch(register(false))}/>
+      <Input type ='reset' value = 'Close' className = "btn cancel" />
     </Form>
   )
 }
