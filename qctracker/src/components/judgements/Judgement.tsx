b0184d5bb664ebb456b914121acfc24104a6a115
Paul <benderp94@gmail.com>
2020-11-06 09:55:23 -0800

    Wrote code for the judgement.tsx portion of the entire judgement component

---
+++ qctracker/src/components/judgements/Judgement.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Button} from 'reactstrap';
+
+
+interface JudgeProps{
+    email: string;
+    username: string;
+    score: number;
+    content: string;
+    vote: number;
+    week: number;
+}
+
+
+export const Judgement: React.FC<JudgeProps> = (props: JudgeProps) => {
+
+    return(
+        <div className="judgement">
+            <div className="judgement-header">
+                <span className="judged-name">
+                    {`${props.username} ${props.week}`}
+                </span>
+            </div>
+            <div className="judgement-body">
+                <p>
+                   {`Anonymous message: ${props.content}`}
+                </p>
+            </div>
+            <div className="judgement-footer">
+                <span>
+                    {props.vote}
+                    <Button outline color="success" className="upvote"> Agree </Button>
+                    <Button outline color="danger" className="upvote"> Disagree </Button>
+                </span>
+            </div>
+        </div>
+    )
+}
---
+++ qctracker/src/components/judgements/JudgementModal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+const modal = () => 
+{
+    return <h1>This the modal bro</h1>;
+}
